fff8878a4f1d7fca9b9a0be33069eaaf23c7e242
bajb <brooke@bajb.net>
2016-02-02 14:54:30 +0000

    Add Cors

--- js.go
+++ js.go
@@ -11,6 +11,9 @@ func init() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	enc := json.NewEncoder(w)
 	d := map[string]string{"ip": r.RemoteAddr, "country": r.Header.Get("X-Appengine-Country"), "region": r.Header.Get("X-Appengine-Region"), "city": r.Header.Get("X-Appengine-City"), "latlong": r.Header.Get("X-Appengine-Citylatlong")}
 	enc.Encode(d)
